deb776d64297b409da8c7ea9edb81702679f42b4
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-22 18:36:22 -0300

    feat(frontend): :iphone: Make Primary feature cards responsive on mobile

--- frontend/components/PrimaryFeatures.tsx
+++ frontend/components/PrimaryFeatures.tsx
@@ -4,7 +4,6 @@ import { useEffect, useId, useRef, useState } from 'react'
 import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
-import { CircleBackground } from '../components/CircleBackground'
 import { Container } from '../components/Container'
 import Image from 'next/image'
 import gpsIcon from '../static/img/gps.svg'
@@ -19,7 +18,7 @@ const features = [
   {
     name: 'Real-Time Location Validation',
     description:
-      'Incorporates GPS and geofencing technologies to validate the exact location of check-in, guaranteeing that individuals are present at the designated site, whether it’s a workplace, classroom, or a remote location.',
+      'Incorporates GPS and geofencing technologies to validate the exact location of check-in, guaranteeing that individuals are present at the designated location.',
     icon: GPSIcon,
   },
   {
@@ -30,9 +29,9 @@ const features = [
   },
 ]
 
-function GPSIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+function GPSIcon(props: React.ComponentPropsWithoutRef<'img'>) {
   return (
-    <Image src={gpsIcon} alt="GPS icon" {...props} width={32} height={32} />
+    <Image src={gpsIcon} alt="GPS icon" {...props} width={64} height={64} />
   )
 }
 
@@ -50,24 +49,6 @@ function DeviceUserIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M5 4a4 4 0 014-4h14a4 4 0 014 4v24a4.002 4.002 0 01-3.01 3.877c-.535.136-.99-.325-.99-.877s.474-.98.959-1.244A2 2 0 0025 28V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 001.041 1.756C8.525 30.02 9 30.448 9 31s-.455 1.013-.99.877A4.002 4.002 0 015 28V4z"
-        fill="#fff"
-      />
-    </svg>
-  )
-}
-
-function DeviceNotificationIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
-      <circle cx={16} cy={16} r={16} fill="#fff" fillOpacity={0.2} />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
-        fill="#fff"
-      />
-      <path
-        d="M9 8a2 2 0 012-2h10a2 2 0 012 2v2a2 2 0 01-2 2H11a2 2 0 01-2-2V8z"
         fill="#fff"
       />
     </svg>
@@ -150,92 +131,54 @@ function FeaturesDesktop() {
 }
 
 function FeaturesMobile() {
-  let [activeIndex, setActiveIndex] = useState(0)
-  let slideContainerRef = useRef<React.ElementRef<'div'>>(null)
-  let slideRefs = useRef<Array<React.ElementRef<'div'>>>([])
-
-  useEffect(() => {
-    let observer = new window.IntersectionObserver(
-      (entries) => {
-        for (let entry of entries) {
-          if (entry.isIntersecting && entry.target instanceof HTMLDivElement) {
-            setActiveIndex(slideRefs.current.indexOf(entry.target))
-            break
-          }
-        }
-      },
-      {
-        root: slideContainerRef.current,
-        threshold: 0.6,
-      },
-    )
-
-    for (let slide of slideRefs.current) {
-      if (slide) {
-        observer.observe(slide)
-      }
-    }
-
-    return () => {
-      observer.disconnect()
-    }
-  }, [slideContainerRef, slideRefs])
+  const [selectedIndex, setSelectedIndex] = useState(0)
 
   return (
-    <>
-      <div
-        ref={slideContainerRef}
-        className="-mb-4 flex snap-x snap-mandatory -space-x-4 overflow-x-auto overscroll-x-contain scroll-smooth pb-4 [scrollbar-width:none] sm:-space-x-6 [&::-webkit-scrollbar]:hidden"
-      >
-        {features.map((feature, featureIndex) => (
-          <div
-            key={featureIndex}
-            ref={(ref) => ref && (slideRefs.current[featureIndex] = ref)}
-            className="w-full flex-none snap-center px-2 sm:px-4"
+    <Tab.Group as="div" className="space-y-4">
+      <Tab.List className="flex space-x-1 rounded-xl bg-purple-500 p-1">
+        {features.map((feature, index) => (
+          <Tab
+            key={feature.name}
+            className={({ selected }) =>
+              clsx(
+                'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-purple-700',
+                'ring-white ring-opacity-60 ring-offset-2 ring-offset-purple-400 focus:outline-none focus:ring-2',
+                selected
+                  ? 'bg-white shadow'
+                  : 'text-purple-100 hover:bg-white/[0.12] hover:text-white',
+              )
+            }
           >
-            <div className="relative transform overflow-hidden rounded-2xl bg-gray-800 px-5 py-6">
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <CircleBackground
-                  color="#13B5C8"
-                  className={featureIndex % 2 === 1 ? 'rotate-180' : undefined}
-                />
+            {feature.name}
+          </Tab>
+        ))}
+      </Tab.List>
+      <Tab.Panels className="mt-2">
+        {features.map((feature, index) => (
+          <Tab.Panel
+            key={feature.name}
+            className={clsx(
+              'rounded-xl p-3',
+              'ring-white ring-opacity-60 ring-offset-2 ring-offset-purple-400 focus:outline-none focus:ring-2',
+            )}
+          >
+            <div className="flex items-start">
+              <div className="flex-shrink-0">
+                <feature.icon className="h-6 w-6" />
               </div>
-
-              <div className="absolute inset-x-0 bottom-0 bg-gray-800/95 p-6 backdrop-blur sm:p-10">
-                <feature.icon className="h-8 w-8" />
-                <h3 className="mt-6 text-sm font-semibold text-white sm:text-lg">
+              <div className="ml-3">
+                <h3 className="text-lg font-semibold text-white">
                   {feature.name}
                 </h3>
-                <p className="mt-2 text-sm text-gray-400">
+                <p className="mt-1 text-sm text-gray-300">
                   {feature.description}
                 </p>
               </div>
             </div>
-          </div>
+          </Tab.Panel>
         ))}
-      </div>
-      <div className="mt-6 flex justify-center gap-3">
-        {features.map((_, featureIndex) => (
-          <button
-            type="button"
-            key={featureIndex}
-            className={clsx(
-              'relative h-0.5 w-4 rounded-full',
-              featureIndex === activeIndex ? 'bg-gray-300' : 'bg-gray-500',
-            )}
-            aria-label={`Go to slide ${featureIndex + 1}`}
-            onClick={() => {
-              slideRefs.current[featureIndex].scrollIntoView({
-                block: 'nearest',
-                inline: 'nearest',
-              })
-            }}
-          >
-            <span className="absolute -inset-x-1.5 -inset-y-3" />
-          </button>
-        ))}
-      </div>
-    </>
+      </Tab.Panels>
+    </Tab.Group>
   )
 }
 
