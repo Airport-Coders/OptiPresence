f1192092e44a930221eb2ccd65ea5d9eac7f7351
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-22 16:59:48 -0300

    refactor(frontend): :heavy_minus_sign: Remove next-css-obfuscator

--- frontend/next-css-obfuscator.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	enable: true,
-	mode: 'random',
-	refreshClassConversionJson: false,
-	allowExtensions: ['.jsx', '.tsx', '.js', '.ts', '.html', '.rsc'],
-
-	blackListedFolderPaths: ['./.next/cache', /\.next\/server\/pages\/api/, /_document..*js/, /_app-.*/, /__.*/],
-};
